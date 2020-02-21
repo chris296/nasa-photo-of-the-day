1d2a01caf19d74825e19dbbdadf94a02b2ff5cb1
chris296 <chris.rios296@gmail.com>
2020-02-20 16:18:21 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import NasaImage from "./components/NasaImage";
 
 function App() {
+  const [photo, setPhoto] = useState({});
+
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=4zBamL4obmRzaKd3bp81sdD0Y1qQLg2CnPd8ev4j')
+        .then(response => {
+            console.log(response.data);
+            setPhoto(response.data);
+        })
+        .catch(error => {
+            console.log('There was an error', error);
+        });
+    }, []);
+    console.log('this is photos',photo)
   return (
+ 
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>Nasa Photo of the Day</h1>
+      <NasaImage photo={photo}/>
     </div>
   );
 }
